34394f52122b9a53b1359d0924597535a605b8e1
Your Name <youexample.com>
2025-05-22 02:53:13 -0500

    lista modificaciones

--- src/Layout/Navegador.jsx
+++ src/Layout/Navegador.jsx
@@ -20,10 +20,9 @@ const Navegador = () => {
           <li><button
             onClick={cerrarSesion}
             className={styles.logoutButton}
-            style={{ background: 'none', border: 'none', cursor: 'pointer' }}
           >
-            <svg className={styles.logautimg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path className= {styles.colorimg}  d="M10 17l5-5-5-5v10zm9-15H5c-1.1 0-2 .9-2 2v4h2V4h14v16H5v-4H3v4c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"  />
+            <svg className={styles.logautimg} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+              <path className= {styles.colorimg}  d="M232-172q-26 0-43-17t-17-43v-114h28v114q0 12 10 22t22 10h496q12 0 22-10t10-22v-496q0-12-10-22t-22-10H232q-12 0-22 10t-10 22v114h-28v-114q0-26 17-43t43-17h496q26 0 43 17t17 43v496q0 26-17 43t-43 17H232Zm206-164-20-20 110-110H172v-28h356L418-604l20-20 144 144-144 144Z"  />
             </svg>
           </button></li>
 
--- src/pages/Vehiculo.jsx
+++ src/pages/Vehiculo.jsx
@@ -11,11 +11,11 @@ const Vehiculo = () => {
   const { auth, loading } = useAuth();
   if (loading) return 'Cargando...';
 
-  
-const { vehiculos, eliminarVehiculo } = useVehiculo();
+
+  const { vehiculos, eliminarVehiculo } = useVehiculo();
 
 
-//------------- CARUCEL
+  //------------- CARUCEL
   const [currentSlide, setCurrentSlide] = useState(0);
   const cardsPerPage = 4;
 
@@ -53,11 +53,11 @@ const { vehiculos, eliminarVehiculo } = useVehiculo();
     <div className={styles.wrapper}>
       <h2 className={styles.heading}>VEHICULOS</h2>
 
-      <div  className= {styles.divAddVehiculo}>
+      <div className={styles.divAddVehiculo}>
         <Agregarvehiculo />
       </div>
 
-    
+
 
       <div className={styles.carouselContainer}>
         <button className={styles.navButton} onClick={prevSlide} disabled={currentSlide === 0}>
@@ -75,8 +75,18 @@ const { vehiculos, eliminarVehiculo } = useVehiculo();
               <p className={styles.cookieDescription}>Tránsito: {vehiculo.transito}</p>
 
               <div className={styles.buttonContainer}>
-                <button className={styles.acceptButton}>Editar</button>
-                <button className={styles.declineButton} onClick={() => eliminarVehiculo(vehiculo.id)} >Eliminar</button>
+
+                <button className={styles.iconOnlyButton} >
+                  <svg className={`${styles.iconButton} ${styles.editar}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path className= {`${styles.colorimgg} ${styles.editar}`} d="M200-200h43.92l427.93-427.92-43.93-43.93L200-243.92V-200Zm-40 40v-100.77l527.23-527.77q6.15-5.48 13.57-8.47 7.43-2.99 15.49-2.99t15.62 2.54q7.55 2.54 13.94 9.15l42.69 42.93q6.61 6.38 9.04 14 2.42 7.63 2.42 15.25 0 8.13-2.74 15.56-2.74 7.42-8.72 13.57L260.77-160H160Zm600.77-556.31-44.46-44.46 44.46 44.46ZM649.5-649.5l-21.58-22.35 43.93 43.93-22.35-21.58Z" />
+                  </svg>
+                </button>
+
+                <button  onClick={() => eliminarVehiculo(vehiculo.id)}  className={styles.iconOnlyButton}>
+                  <svg className={`${styles.iconButton} ${styles.eliminar}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path className= {`${styles.colorimgg} ${styles.eliminar}`} d="M312-172q-25 0-42.5-17.5T252-232v-488h-40v-28h148v-28h240v28h148v28h-40v488q0 26-17 43t-43 17H312Zm368-548H280v488q0 14 9 23t23 9h336q12 0 22-10t10-22v-488ZM402-280h28v-360h-28v360Zm128 0h28v-360h-28v360ZM280-720v520-520Z" />
+                  </svg>
+                </button>
               </div>
             </div>
           ))}
